f3a39459e456556117b4f02411a8d36f291b74b6
Ben Shaw <benshaw9@gmail.com>
2015-09-22 20:59:11 -0700

    22_sept_2015 backup, count: 1456

---
+++ process.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+lines = []
+
+with open("raw_22_sep_2015_backup.txt", 'r') as f:
+    for line in f:
+        line = line.translate(None, '\n')
+        split_line = line.split(' – ')
+        if len(split_line) > 2:
+            print line
+        switched_line = split_line[-1] + " – " + " – ".join(split_line[:-1])
+        lines.append(switched_line)
+
+# lines = sorted(lines)
+# for line in lines:
+#     print line
