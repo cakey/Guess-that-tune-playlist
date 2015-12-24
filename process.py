ec719ad4640caf85be1ca5c1ad0f0cc72f8db03e
Benjamin Shaw <b.shaw@b-shaw-ltm.internal.salesforce.com>
2015-12-24 04:26:27 -0800

    24 dec backup+analysis

--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 lines = []
 
-with open("raw_22_sep_2015_backup.txt", 'r') as f:
+with open("raw_24_dec_2015_backup.txt", 'r') as f:
     for line in f:
         line = line.translate(None, '\n')
         split_line = line.split(' – ')
@@ -10,6 +10,6 @@ with open("raw_22_sep_2015_backup.txt", 'r') as f:
         switched_line = split_line[-1] + " – " + " – ".join(split_line[:-1])
         lines.append(switched_line)
 
-# lines = sorted(lines)
-# for line in lines:
-#     print line
+lines = sorted(lines)
+for line in lines:
+    print line
